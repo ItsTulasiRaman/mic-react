c6fe50198675dc7d819282afbdfc1d2a49a6020b
Emmanuel Allan <90669430+EmmanuelAllanMJ@users.noreply.github.com>
2022-10-28 00:59:41 +0530

    Modified mob view of cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,7 @@ import homeCardsArrow from "./ui/homeCardsArrow.png";
 const Home = () => {
   return (
     <div className="home-background">
-
-      <div className="grid grid-rows-[500px_200px] md:grid-rows-1 md:grid-cols-[1fr_1fr_500px] lg:grid-cols-[1fr_1fr_1fr_700px] md:gap-10 items-center   w-100 h-screen ">
-
+      <div className="grid grid-rows-[500px_200px] grid-cols-[100vw] md:grid-rows-1 md:grid-cols-[1fr_1fr_500px] lg:grid-cols-[1fr_1fr_1fr_700px] md:gap-10 items-center   w-100 h-screen ">
         <div className="flex flex-col h-34 w-145 m-auto  text-center md:text-left md:p-[10%] md:col-span-2 lg:col-span-3">
           <div className="text-4xl md:text-5xl lg:text-6xl font-extrabold ">
             <h1>Together We Learn</h1>
@@ -28,7 +26,6 @@ const Home = () => {
 
         <div className=" md:flex relative  items-center  ">
           <div className=" max-w-sm md:max-w-none m-[10%] md:w-1/2 md:ml=[20%] md:ml-10 lg:ml-[50px] box-shad ">
-
             <img
               src={cardMoviesCode}
               alt="Hero cards for home page code "
@@ -43,8 +40,7 @@ const Home = () => {
             />
           </div>
 
-          <div className="max-w-sm md:max-w-none absolute w-9/12 md:w-1/2 top-[250px] right-[0px] md:top-40 md:right-40 lg:top-[250px] right-[200px] z-10 w-1/2 ">
-
+          <div className="max-w-sm  md:max-w-none absolute w-[70%] top-[70%] right-[10%] md:w-1/2  md:top-40 md:right-40 lg:top-[250px] right-[200px] z-10 w-1/2 ">
             <img src={cardMoviesResult} alt="Hero cards for home page result" />
           </div>
         </div>
@@ -53,6 +49,4 @@ const Home = () => {
   );
 };
 
-
 export default Home;
-
